351ae728359ae94b64e6685ea8c52aeb8177e488
unknown <quinchej659@gmail.com>
2022-10-25 12:00:44 -0500

    chiquitrosfs

---
+++ src/Git.java
@@ -0,0 +1,26 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("COMANDOS DE GIT");
+        System.out.println("it clone <https: // link -con-nombre-del-repositorio>");
+        System.out.println("git branch <nombre-de-la-rama>");
+        System.out.println("git push <nombre-remoto> <nombre-rama>");
+        System.out.println("git branch");
+        System.out.println("git branch --list");
+        System.out.println("git branch -d <nombre-de-la-rama>");
+        System.out.println("git checkout <nombre-de-la-rama>");
+        System.out.println("git checkout -b <nombre-de-tu-rama>");
+        System.out.println("git status");
+        System.out.println("git add <archivo>");
+        System.out.println("git add -A");
+        System.out.println("git commit -m \"mensaje de confirmación\"");
+        System.out.println("git push <nombre-remoto> <nombre-de-tu-rama>");
+        System.out.println("git push --set-upstream <nombre-remoto> <nombre-de-tu-rama>");
+        System.out.println("git push -u origin <nombre-de-tu-rama>");
+        System.out.println("git pull <nombre-remoto>");
+        System.out.println("git revert 3321844");
+        System.out.println("git checkout dev");
+        System.out.println("git fetch");
+        System.out.println("git merge <nombre-de-la-rama>");
+
+    }
+}
